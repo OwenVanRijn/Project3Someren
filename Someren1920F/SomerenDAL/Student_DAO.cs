37f7c1a7a0768bfa4c7a5821a4db3bfa0957b552
Schotsl <sjorsvanholst@gmail.com>
2020-03-18 16:31:05 +0100

    Removed unused imports

--- Someren1920F/SomerenDAL/Student_DAO.cs
+++ Someren1920F/SomerenDAL/Student_DAO.cs
@@ -1,11 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Data;
-using System.Collections.ObjectModel;
 using SomerenModel;
 
 namespace SomerenDAL
--- Someren1920F/SomerenLogic/Lecturers_Service.cs
+++ Someren1920F/SomerenLogic/Lecturers_Service.cs
@@ -2,10 +2,6 @@
 using SomerenModel;
 using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SomerenLogic
 {
